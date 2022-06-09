ad86877b2e7b6b13b64f346b9cd9364ea2f31d33
Ryan Brisch <brischster@gmail.com>
2022-06-09 17:26:53 -0600

    got some new queries completed and working on some inquirer questions for adding new columns

--- server.js
+++ server.js
@@ -34,7 +34,22 @@ const mainMenu = async () => {
       displayDepartments();
       break;
     case "Display Roles":
-      //displayRoles()
+      displayRoles();
+      break;
+    case "Display Employees":
+      displayEmployees();
+      break;
+    case "Add a Department":
+      //addDepartment();
+      break;
+    case "Add a Role":
+      //addRole();
+      break;
+    case "Add an Employee":
+      //addEmployee();
+      break;
+    case "Update an Employee Role":
+      //updateEmployeeRole();
       break;
     default:
       process.exit(0);
@@ -56,5 +71,41 @@ const displayRoles = async () => {
   console.table(data[0]);
   mainMenu();
 };
+
+const displayEmployees = async () => {
+  const data = await db.promise().query(
+    // "SELECT first_name, last_name, job_role.title, job_role.salary FROM employee LEFT JOIN job_role ON role_id = job_role.id LEFT JOIN department ON department_id = department.id GROUP BY "
+    // //LEFT JOIN employee ON manager_id = employee.id"
+
+    "SELECT first_name, last_name, job_role.title, job_role.salary FROM employee LEFT JOIN (job_role, department) ON (role_id = job_role.id AND department_id = department.id)"
+  );
+  console.table(data[0]);
+  mainMenu();
+};
+
+const addDepartment = async () => {
+  const data = await db.promise().query("ALTER TABLE department ADD ");
+};
+
+const addDeptQuestion = [
+  {
+    type: "input",
+    message: "What is the name of the department you would like to add?",
+    name: "newDept",
+  },
+];
+
+const addRoleQuestions = [
+  {
+    type: "input",
+    message: "What is the title of the role you would like to add?",
+    name: "newRole",
+  },
+  {
+    type: "input",
+    message: "What is the salary of the new role?",
+    name: "newSalary",
+  },
+];
 
 mainMenu();
